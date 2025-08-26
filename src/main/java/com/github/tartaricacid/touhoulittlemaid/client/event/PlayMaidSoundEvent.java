1a4447106de399ac5beea98698146aeb048a647d
QiChenSn <2102733232@qq.com>
2025-08-26 20:44:59 +0800

    Merge pull request #840
    * 修复神社亭灯由物品放置时无法正确读取数据的问题
    
    * 1.新增在指定坐标播放女仆声音;2.让鹦鹉可以模仿女仆说话
    
    * Merge remote-tracking branch 'origin/1.21' into 1.21
    
    * 修正部分内容
    
    * Merge branch '1.21' into 1.21-new-parrot_in_maid_mouth

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/PlayMaidSoundEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/PlayMaidSoundEvent.java
@@ -1,6 +1,8 @@
 package com.github.tartaricacid.touhoulittlemaid.client.event;
 
+import com.github.tartaricacid.touhoulittlemaid.api.client.sound.ICustomSoundBuffer;
 import com.github.tartaricacid.touhoulittlemaid.client.sound.data.MaidSoundInstance;
+import com.github.tartaricacid.touhoulittlemaid.client.sound.data.MaidSoundInstanceAtPos;
 import com.mojang.blaze3d.audio.SoundBuffer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,8 +13,8 @@ import net.neoforged.neoforge.client.event.sound.PlaySoundSourceEvent;
 public class PlayMaidSoundEvent {
     @SubscribeEvent
     public static void onPlaySoundSource(PlaySoundSourceEvent event) {
-        if (event.getSound() instanceof MaidSoundInstance instance) {
-            SoundBuffer soundBuffer = instance.getSoundBuffer();
+        if (event.getSound() instanceof ICustomSoundBuffer custom) {
+            SoundBuffer soundBuffer = custom.getSoundBuffer();
             if (soundBuffer != null) {
                 event.getChannel().attachStaticBuffer(soundBuffer);
                 event.getChannel().play();
