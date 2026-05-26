1227f48349ff88ee0717990d7617f4b9adbe835e
mikasa <1244456115@qq.com>
2026-05-26 14:43:26 +0800

    修正MobInteractMixin

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/MobInteractMixin.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/MobInteractMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,11 +28,9 @@ import java.util.List;
  */
 @Mixin(value = Mob.class, priority = 999)
 public class MobInteractMixin {
-
-    @SuppressWarnings("all")
     @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
-    private void touhouLittleMaid$RightClick(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
-        if (!((Object) this instanceof EntityMaid maid)) {
+    private void touhouLittleMaid$RightClick(Player player, InteractionHand hand, Vec3 location, CallbackInfoReturnable<InteractionResult> cir) {
+        if (!((Mob) (Object) this instanceof EntityMaid maid)) {
             return;
         }
         if (hand != InteractionHand.MAIN_HAND) {
