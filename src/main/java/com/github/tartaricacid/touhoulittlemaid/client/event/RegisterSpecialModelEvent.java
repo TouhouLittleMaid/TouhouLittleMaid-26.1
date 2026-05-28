47f9578a658621f0065c61f2658b3e8007be8e37
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-05-28 16:19:03 +0800

    准备野餐篮渲染

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/RegisterSpecialModelEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/RegisterSpecialModelEvent.java
@@ -2,6 +2,7 @@ package com.github.tartaricacid.touhoulittlemaid.client.event;
 
 import com.github.tartaricacid.touhoulittlemaid.client.renderer.tileentity.TileEntityItemStackChairRenderer;
 import com.github.tartaricacid.touhoulittlemaid.client.renderer.tileentity.TileEntityItemStackGarageKitRenderer;
+import com.github.tartaricacid.touhoulittlemaid.client.renderer.tileentity.TileEntityItemStackPicnicBasketRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -13,5 +14,6 @@ public class RegisterSpecialModelEvent {
     public static void registerSpecialModelRenderers(RegisterSpecialModelRendererEvent event) {
         event.register(TileEntityItemStackChairRenderer.CHAIR_ITEM_RENDERER, TileEntityItemStackChairRenderer.Unbaked.MAP_CODEC);
         event.register(TileEntityItemStackGarageKitRenderer.GARAGE_KIT_ITEM_RENDERER, TileEntityItemStackGarageKitRenderer.Unbaked.MAP_CODEC);
+        event.register(TileEntityItemStackPicnicBasketRenderer.PICNIC_BASKET_ITEM_RENDERER, TileEntityItemStackPicnicBasketRenderer.Unbaked.MAP_CODEC);
     }
 }
